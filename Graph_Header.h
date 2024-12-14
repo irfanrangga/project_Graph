33add29cc49060323b96a40e05308ac9597de506
hannndii <shiirogami95@gmail.com>
2024-12-14 18:32:55 +0700

    Nih tinggal 3 fitur doang

--- Graph_Header.h
+++ Graph_Header.h
@@ -4,39 +4,45 @@
 #define firstKota(G) G.firstKota
 #define idKota(v) v->idKota
 #define nextKota(v) v->nextKota
-#define firstDestination(v) v->firstDestination
+#define firstDest(v) v->firstDest
 #define destKotaID(e) e->destKotaID
 #define weight(e) e->weight
-#define nextDestination(e) e->nextDestination
+#define nextDest(e) e->nextDest
 using namespace std;
 
 //Kota = Vertex
 typedef struct Kota *adrKota;
-//Destination = Edge
-typedef struct Destination *adrDestination;
+//Dest = Edge
+typedef struct Dest *adrDest;
 
 struct Kota {
     string idKota;
     adrKota nextKota;
-    adrDestination firstDestination;
+    adrDest firstDest;
 };
 
-struct Destination {
+struct Dest {
     string destKotaID;
     int weight;
-    adrDestination nextDestination;
+    adrDest nextDest;
 };
 
 struct Map {
     adrKota firstKota;
 };
 
-void createKota(string newKotaID, adrKota &v);
+void createKota(string newKotaId, adrKota &v);
+void createDest(string destKotaID, int weight, adrDest &d);
 void initMap(Map &G);
 void addKota(Map &G, string newKotaID);
+void addDest(Map &G, string sourceKotaID, string destKotaID, int weight);
+void inputDests(Map &G, string sourceKotaID, string tujuanAkhir);
 void buildMap(Map &G);
-//void printMap(Map &G);
+void printMap(Map &G);
+int indegree(Map G, string kotaID);
+int outdegree(Map G, string kotaID);
+int degree(Map G, string kotaID);
 //void findFastestRoute(Map G);
 //void findCheapestFare(Map G);
-//int caculateRoute(Map G);
+//int caculateRoute(MapÂ G);
 #endif
--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include "Graph_Header.h"
 
-int main()
-{
+int main() {
     Map G;
+    string tujuanAkhir = "Budapest";
+
+    initMap(G);
+
     buildMap(G);
 
-    adrKota v = firstKota(G);
-    cout << endl;
-    while(v != nullptr){
-        cout << idKota(v) << " " << endl;
-        v = nextKota(v);
-    }
+    cout << "\nPeta Koneksi Antar Kota: " << endl;
+    printMap(G);
 
-    cout << endl;
+
+    string kotaID;
+
+    // Test indegree, outdegree, and degree for different cities
+    cout << "Masukkan nama kota untuk uji indegree, outdegree, dan degree: ";
+    cin >> kotaID;
+
+    int indeg = indegree(G, kotaID);
+    int outdeg = outdegree(G, kotaID);
+    int deg = degree(G, kotaID);
+
+    cout << "Indegree kota " << kotaID << " = " << indeg << endl;
+    cout << "Outdegree kota " << kotaID << " = " << outdeg << endl;
+    cout << "Degree kota " << kotaID << " = " << deg << endl;
+
     return 0;
 }
